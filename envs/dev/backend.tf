b5371f56f9e6f953a93fb433cf4c63b6ab5db22e
bhushan04ec041 <bhushan04ec041@gmail.com>
2025-08-10 11:42:26 +0000

    added backend

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state-bucket-for-terraform"
-    key            = "dev/terraform.tfstate"
+    key            = "envs/${terraform.workspace}/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
